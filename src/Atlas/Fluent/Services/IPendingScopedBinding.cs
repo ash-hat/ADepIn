395b9364bd3caabd5023eabcaaaaaa0bb0c9bcc7
Ash <ashe.null@protonmail.ch>
2020-11-13 23:35:10 -0600

    uninherit pending for pendingscoped it showed extension methods that werent applicable

--- src/Atlas/Fluent/Services/IPendingScopedBinding.cs
+++ src/Atlas/Fluent/Services/IPendingScopedBinding.cs
@@ -2,13 +2,19 @@ namespace Atlas.Fluent
 {
 	/// <summary>
 	/// 	Represents a binding that has been created and scoped but not yet bound to a <see cref="IServiceBinder" />.
+	/// 	<p>This interface composites <see cref="IPendingScopedBinding{TService, TContext}.Applicator"/> to improve ease of extension method usage.</p>
 	/// </summary>
 	/// <typeparam name="TService">The type of service to produce.</typeparam>
 	/// <typeparam name="TContext">The type of context to consume.</typeparam>
-	public interface IPendingScopedBinding<TService, TContext> : IPendingBinding<TService, TContext>
+	public interface IPendingScopedBinding<TService, TContext>
 		where TService : notnull
 		where TContext : notnull
 	{
+		/// <summary>
+		/// 	The function to set the binding.
+		/// </summary>
+		PendingApplicator<TService, TContext> Applicator { get; }
+
 		/// <summary>
 		/// 	The binding awaiting to be bound.
 		/// </summary>
